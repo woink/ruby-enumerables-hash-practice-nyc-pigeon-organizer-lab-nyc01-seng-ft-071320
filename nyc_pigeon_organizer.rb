86ab4a7deb4b99b40ca16eb1e78cc2934e9a74ae
Ward Price <wardprice@gmail.com>
2020-07-01 13:46:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
    value.each do |inner_key, names|
     binding.pry
+    names.each do 
     end
   final_array
   end
